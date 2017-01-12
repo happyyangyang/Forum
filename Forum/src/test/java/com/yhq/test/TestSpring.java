dafc32dd32fc71edd7c40a7391304121fdd412d7
yhq <yhq0319abc@163.com>
2017-01-12 09:59:19 +0800

    测试git使用

--- Forum/src/test/java/com/yhq/test/TestSpring.java
+++ Forum/src/test/java/com/yhq/test/TestSpring.java
@@ -3,6 +3,10 @@ package com.yhq.test;
 public class TestSpring {
 	public void show(){
 		System.out.println("从git下载，然后再提交");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		
 	}
 
 }
